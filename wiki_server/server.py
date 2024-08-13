22f2479ea3344f67cc63676fb4f60aede08b093a
epeters-jrmndgndr <contact@emilpetersen.me>
2024-08-12 21:16:30 -0400

    Set CORS rules to allow localhost

--- wiki_server/server.py
+++ wiki_server/server.py
@@ -3,7 +3,22 @@ from typing import Any
 from fastapi import FastAPI, Response, Body, Request, status
 from fastapi.responses import HTMLResponse
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 stored_articles = {}
 
